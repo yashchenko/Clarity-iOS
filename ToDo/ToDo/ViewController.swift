37f9c1f61cd4a1f56736f3f8a74ec7caefb38ff8
Ivan Yashchenko <ivan.profitx@gmail.com>
2025-10-31 00:36:48 +0200

    feat: Initial project setup with MVVM-C structure and Core Data

---
+++ ToDo/ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Ivan on 31.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+        
+    }
+
+
+}
